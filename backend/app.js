7e4404d721d607e2b15fac63e4d2bd2628389802
LykaJ <raulet.alicia@gmail.com>
2020-08-12 14:58:40 +0200

    init

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const dbUrl = require('./dbUrl');
+
+mongoose.connect(dbUrl,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to MongoBD!'))
+    .catch(() => console.log('Connection to MongoDB failed.'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+module.exports = app;
