aabf2ddd281fef1894db2bfe7b07151d8b412bb0
LykaJ <raulet.alicia@gmail.com>
2020-09-10 10:40:29 +0200

    code for the auth middleware was reworked to avoid the headers error. Though the check on token validity not working

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 
 export default class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         let email;
         let status;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import Product from "./Product";
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <div className="container">
